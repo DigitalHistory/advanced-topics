f54a127f16604262469be092a74d1b24a06008af
Matt Price <matt.price@utoronto.ca>
2015-07-21 08:52:14 -0400

    typo and formatting fixes; also some additional documentation.

--- script.js
+++ script.js
@@ -8,56 +8,64 @@ var my_markers = []; // we use this in the main loop below to hold the markers
 // this one is strange.  In google maps, there is usually only one
 // infowindow -- its content and position change when you click on a 
 // marker
-var infowindow = new google.maps.InfoWindow({content: ""}); 
+var infowindow = new google.maps.InfoWindow({content: ""});
 
-var my_first_marker; // we'll use this to create one marker by hand.  
+var my_first_marker; // we'll use this to create a marker by hand.
 
 /* a function that will run when the page loads.  It creates the map
-  and grabs the marker data fom the spreadsheet */
+ and the initial marker.  If you want to create more markers, do it here. */
 function initialize() {
-  my_map_options = {
-    center:  my_center, // to change this value, change my_center above
-          zoom: 13,  // higher is closer-up
-          mapTypeId: google.maps.MapTypeId.HYBRID // you can also use TERRAIN, STREETMAP, SATELLITE
-        };
-  
-  // this one line creates the actual map
-  my_map = new google.maps.Map(document.getElementById("map_canvas"),
-                               my_map_options);
-  
-  // This is our first, hand-crafted map marker  
-  // Modify all its attributes to replace it with a new marker
-  // copy and rename it to create a second marker
-  my_first_marker = new google.maps.Marker({
-    position: my_center,
-    map: my_map,
-    title: "First Marker",
-    window_content:"<h1>This is the title</h1><p> and this would be the extended description</p>"
-  }); 
-  my_markers.push({marker:my_first_marker, listener: my_first_listener}); 
-  
-  // this "listener" checks for mouse clicks and opens the info window
-  // you will have to copy, rename, and lighlty modify it if you create a 
-  // second marker
-  var my_first_listener = google.maps.event.addListener(my_first_marker, 'click', function() {
-      infowindow.setContent (this.window_content);
-      infowindow.open(my_map, this); 
+    my_map_options = {
+        center:  my_center, // to change this value, change my_center above
+        zoom: 13,  // higher is closer-up
+        mapTypeId: google.maps.MapTypeId.HYBRID // you can also use TERRAIN, STREETMAP, SATELLITE
+    };
+    
+    // this one line creates the actual map
+    my_map = new google.maps.Map(document.getElementById("map_canvas"),
+                                 my_map_options);
+
+    /* marker definition begins here! to create a new marker, copy/paste and change values */
+    /* ----------------------------------------------------------------------------------- */
+
+    // This is our first, hand-crafted map marker  
+    // Modify all its attributes to replace it with a new marker
+    // copy and rename it to create a second marker
+    my_first_marker = new google.maps.Marker({
+        position: my_center,
+        map: my_map,
+        title: "First Marker",
+        window_content:"<h1>This is the title</h1><p> and this would be the extended description</p>"
+    }); 
+    
+    // this "listener" checks for mouse clicks and opens the info window
+    // you will have to copy, rename, and lighlty modify it if you create a 
+    // second marker
+    var my_first_listener = google.maps.event.addListener(my_first_marker, 'click', function() {
+        infowindow.setContent (this.window_content);
+        infowindow.open(my_map, this); 
     });
-  
+
+    // add this new marker to the marker array, for our buttons. 
+    my_markers.push({marker:my_first_marker, listener: my_first_listener}); 
+
+    /* ----------------------------------------------------------------------------------- */
+    /* end marker definition!! Be sure to paste new marker code ABOVE the "}" just below this */
+
 }
 
 // this hides all markers in the array
 // passed to it, by attaching them to 
 // an empty object (instead of a real map)
 function hideMarkers (marker_array) {
-  for (var j in marker_array) {
-    marker_array[j].marker.setMap(null);
-  }
+    for (var j in marker_array) {
+        marker_array[j].marker.setMap(null);
+    }
 }
 // by contrast, this attaches all the markers to 
 // a real map object, so they reappear
 function showMarkers (marker_array, map) {
     for (var j in marker_array) {
-      marker_array[j].marker.setMap(map);
-  }
+        marker_array[j].marker.setMap(map);
+    }
 }
