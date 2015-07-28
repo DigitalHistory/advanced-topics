1deabcbc8b8ff7bc8c193e162aeaa48a3d033b83
Matt Price <matt.price@utoronto.ca>
2015-07-28 14:04:21 -0400

    Fix Closure error in for loop
    replace references to scoped objects with "this".

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // initialize the variables we need
-// we do this here to make sure we can access them 
+// we do this here to make sure we can access them
 // whenever we need to -- they have 'global scope'
 var my_map; // this will hold the map
-var my_map_options; // this will hold the options we'll use to create the map 
-var my_center = new google.maps.LatLng(43.653963,-79.389954); // center of map
+var my_map_options; // this will hold the options we'll use to create the map
+var my_center = new google.maps.LatLng(41.9000,12.5000); // center of map
 var my_markers = []; // we use this in the main loop belowa to hold the markers
 // this one is strange.  In google maps, there is usually only one
-// infowindow -- its content and position change when you click on a 
+// infowindow -- its content and position change when you click on a
 // marker
 var infowindow = new google.maps.InfoWindow({content: ""});
 
@@ -20,47 +20,56 @@ function initialize() {
         zoom: 13,  // higher is closer-up
         mapTypeId: google.maps.MapTypeId.HYBRID // you can also use TERRAIN, STREETMAP, SATELLITE
     };
-    
+
     // this one line creates the actual map
     my_map = new google.maps.Map(document.getElementById("map_canvas"),
                                  my_map_options);
     // this is an *array* that holds all the marker info
     var all_my_markers =
-            [{position: new google.maps.LatLng(43.653963,-79.389954),
+            [{position: new google.maps.LatLng(41.9000,12.5000),
               map: my_map,
               title: "first Marker",
               window_content: "<h1>Marker1</h1><p> and this would be the extended description</p>"
              },
-             {position: new google.maps.LatLng(43.652843,-79.399952),
+             {position: new google.maps.LatLng(41.8902,12.4923),
               map: my_map,
               title: "second Marker",
               window_content: "<h1>Marker2</h1><p> and this would be the extended description</p>"
-             }];
+            },
+            {position: new google.maps.LatLng(41.8986,12.4768),
+             map: my_map,
+             title: "third Marker",
+             window_content: "<h1>Marker3</h1><p> and this would be the extended description</p>"
+           }
+            ];
 
     for (j = 0; j < all_my_markers.length; j++) {
-        var this_marker =  new google.maps.Marker({
+        var marker =  new google.maps.Marker({
             position: all_my_markers[j].position,
             map: my_map,
             title: all_my_markers[j].title,
             window_content: all_my_markers[j].window_content});
-        var this_listener = google.maps.event.addListener(this_marker, 'click', function() {
-            infowindow.setContent (this_marker.window_content);
-            infowindow.open(my_map, this_marker); 
+        marker.info = new google.maps.InfoWindow({content: marker.window_content});
+        var listener = google.maps.event.addListener(marker, 'click', function() {
+            // if you want to allow multiple info windows, uncomment the next line
+            // and comment out the two lines that follow it
+            //this.info.open(this.map, this);
+            infowindow.setContent (this.window_content);
+            infowindow.open(my_map, this);
         });
-        my_markers.push({marker:this_marker, listener: this_listener});
-    };
-
+        my_markers.push({marker:marker, listener:listener});
+    }
 }
 
 // this hides all markers in the array
-// passed to it, by attaching them to 
+// passed to it, by attaching them to
 // an empty object (instead of a real map)
 function hideMarkers (marker_array) {
     for (var j in marker_array) {
         marker_array[j].marker.setMap(null);
     }
 }
-// by contrast, this attaches all the markers to 
+// by contrast, this attaches all the markers to
 // a real map object, so they reappear
 function showMarkers (marker_array, map) {
     for (var j in marker_array) {
