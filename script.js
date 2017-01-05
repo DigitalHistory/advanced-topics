13da7fdf3bfd6493ad67ed5373dea617d593d55d
Matt Price <matt.price@utoronto.ca>
2017-01-04 21:10:11 -0500

    Improve readability, add GeoJSON capability.

--- script.js
+++ script.js
@@ -6,17 +6,37 @@ var my_map_options; // this will hold the options we'll use to create the map
 var my_center = new google.maps.LatLng(41.9000,12.5000); // center of map
 var my_markers = []; // we use this in the main loop below to hold the markers
 // this one is strange.  In google maps, there is usually only one
-// infowindow -- its content and position change when you click on a
-// marker
+// infowindow object -- its content and position change when you click on a
+// marker.  This is counterintuitive, but we need to live with it.  
 var infowindow = new google.maps.InfoWindow({content: ""});
-var legendHTML = "";
+var legendHTML = "<h1>Lookout! This is the Legend!</h1>";
 
 // I'm complicating things a bit with this next set of variables, which will help us
-// with marker colors
+// to make multi-colored markers
 var blueURL = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
 var redURL = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
 var red_markers = [];
 var blue_markers = [];
+
+// this is for fun, if you want it.  With this powerful feature you can add arbitrary
+// data layers to your map.  It's cool. Learn more at:
+// https://developers.google.com/maps/documentation/javascript/datalayer#load_geojson
+var myGeoJSON= {
+  "type":"FeatureCollection",
+  "features":
+  [{"type":"Feature",
+    "properties":{},
+    "geometry":{"type":"Polygon",
+                "coordinates":[[[-85.60546875,49.03786794532644],[-96.6796875,40.713955826286046],
+                                [-79.62890625,37.71859032558816],[-81.2109375,49.26780455063753],
+                                [-85.60546875,49.03786794532644]]]}},
+   {"type":"Feature",
+    "properties":{},
+     "geometry":{"type":"Polygon",
+                 "coordinates":[[[-113.203125,58.35563036280967],[-114.78515624999999,51.944264879028765],
+                                 [-101.6015625,51.944264879028765],[-112.32421875,58.263287052486035],
+                                 [-113.203125,58.35563036280967]]]
+                }}]};
 
 
 /* a function that will run when the page loads.  It creates the map
@@ -50,7 +70,10 @@ function initialize() {
              map: my_map,
              icon: redURL, // this sets the image that represents the marker in the map
              title: "third Marker",
-             window_content: "<h1>Marker3</h1><p> and this would be the extended description</p>"
+             window_content: '<h1>Marker3</h1><p> ' +
+             'and this would be the extended description. ' +
+             'and here is a blockquote:</p><img title="Picture of Quote. Src: someone, some year"  src="htp://somewhere"/>' +
+             '<blockquote>quote quote quote quote</blockquote>'
            }
             ];
 
@@ -64,7 +87,9 @@ function initialize() {
 
         // this next line is ugly, and you should change it to be prettier.
         // be careful not to introduce syntax errors though.  
-        legendHTML += "<div class=\"pointer\" onclick=\"locateMarker(my_markers[" + j + "])\"> <h3>" + marker.title + "</h3><div>" + marker.window_content + "</div></div>";
+      legendHTML +=
+        "<div class=\"pointer\" onclick=\"locateMarker(my_markers[" + j + "])\"> " +
+         "<h3>" + marker.title + "</h3><div>" + marker.window_content + "</div></div>";
         marker.info = new google.maps.InfoWindow({content: marker.window_content});
         var listener = google.maps.event.addListener(marker, 'click', function() {
             // if you want to allow multiple info windows, uncomment the next line
@@ -82,7 +107,7 @@ function initialize() {
         
     }
     document.getElementById("map_legend").innerHTML = legendHTML;
-   
+    my_map.data.addGeoJson(myGeoJSON);   
 }
 
 // this hides all markers in the array
