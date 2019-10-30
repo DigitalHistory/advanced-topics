7d96df44b5a4031028676c4ca7f50a616d64ddd5
Matt Price <matt.price@utoronto.ca>
2019-10-30 12:43:34 -0400

    static-parts: fix menu creation

--- js/static-parts.js
+++ js/static-parts.js
@@ -17,24 +17,15 @@ let authorName='YOUR NAME HERE',
     footerHtml=`HTML projects by ${authorName}, originally written for <a href="https://digitalhistory.github.io/dh-website/">HIS393: Digital History</a>`;
 
 
-function makeMenu (items= menuEntries, path = location.pathname) {
-  let allPaths = [],
-      prefix = '',
+function makeMenu (items= menuEntries) {
+  let prefix = '',
       html = '',
-      basedir = window.location.pathname.split(/\\\//g).splice(-2)[0]; // check which dir we're in
-  if (basedir !== 'advanced-topics') {
+      basedir = window.location.pathname.split(/\//g).splice(-2)[0]; // check which dir we're in
+    // console.log("checking location");
+    // console.log(basedir);
+    // console.log(window.location.pathname)
+    if (! (basedir === 'advanced-topics' || basedir === "")) {
     prefix = '../';
-  }
-  for (let i of items) {
-    if (i.link !== 'index.html') {
-      allPaths.push(i.link);
-    }
-  }
-  for (let p of allPaths) {
-    // if (path.includes(p)) {
-    //   prefix= '../';
-    //   break;
-    // }
   }
   for (let i of items) {
     html += `<a href="${prefix}${i.link}">${i.text}</a>`;
@@ -42,7 +33,7 @@ function makeMenu (items= menuEntries, path = location.pathname) {
   html = '<div class="nav-right">' + html + "</div>";
   $('header.nav').append(html);
 }
-
+ 
 function makeFooter (html) {
   $('footer#page-footer').html(`<main>${html}</main>`);
 }
